6e1233b79a0fc0dc630969707801f6e580a363b0
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2014-05-13 11:04:27 +0200

    added panel administrator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'dashboard#index'
+  end
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
