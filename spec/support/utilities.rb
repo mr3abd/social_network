9da08da4028056c018b6366b73a87a3ac5705da9
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2014-10-25 09:00:55 +0200

    Replacing the full_tilte test helper with a simple include

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+=begin
 def full_title(page_title)
   base_title = "Ruby on Rails Social Network 2014"
   if page_title.empty?
@@ -5,4 +7,5 @@ def full_title(page_title)
   else
     "#{base_title} | #{page_title}"
   end
-end  
+end
+=end  
