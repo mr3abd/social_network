e4185848cf00cbad18aec6d05b19e10b45841606
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2014-11-24 14:54:44 +0100

    Added test for the sign in

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Antonino Scaffidi"
+    email     "antoninoscaffidi@example.com"
+    password  "foobar"
+    password_confirmation  "foobar"
+  end
+end
