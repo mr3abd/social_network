de3f5bfcbcc5c98302eea53b955c31a5d550dd0c
Antonino Scaffidi <antoninoscaffidi@gmail.com>
2014-11-24 16:30:32 +0100

    Update test for signup and signin pages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,14 +3,25 @@ require 'spec_helper'
 describe "User Pages" do
   subject {page}
   
+  describe "account page" do
+    let(:user) {FactoryGirl.create(:user)}
+    before {visit user_path(user)}
+   
+    it {should have_content(user.name)}
+    it {should have_title(user.name)}
+    #it {should have_content('Sign up')}
+    #it {should have_title(full_title('Sign up'))}
+ 
+  end
+   
   describe "signup page" do
     before {visit signup_path}
     
     it {should have_content('Sign up')}
     it {should have_title(full_title('Sign up'))}
- 
-  end
-   
+    
+  end 
+  
 
   describe "signup" do
     before {visit signup_path}
